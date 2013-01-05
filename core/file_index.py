b7cd433a52ee4f92e1b94d090b0a1984516a5cfd
Sebastian Kruse <s.t.kruse@gmx.de>
2013-01-05 10:45:47 +0100

    Simple traversal and saving of a directory structure.

--- core/file_index.py
+++ core/file_index.py
@@ -1,17 +1,54 @@
 import pickle, os
 
-cache_filename = ".syncguy.db"
+cache_filename = ".syncguy.pickle"
 pickle_protocol = 2
 
+class Directory(object):
+	def __init__(self, name=None):
+		self.name = name
+		self.files = {}
+		self.dirs = {}
+		
+	def add_file(self, file):
+		self.files[file.name] = file
+		
+	def add_directory(self, dir):
+		self.dirs[dir.name] = dir
+		
+	def get_dir(self, name):
+		dir = self.dirs.get(name, None)
+		if dir is None:
+			dir = Directory(name)
+			self.add_directory(dir)
+		return dir
+		
+class File(object):
+	def __init__(self, name, mod_time, size, content):
+		self.name = name
+		self.mod_time = mod_time
+		self.size = size
+		self.content = content
+		
+	def __repr__(self):
+		return str(self)
+		
+	def __str__(self):
+		return "File('"+self.name+"')"
+
 class CachedFileIndex(object):
-	__slots__ = ["__root_dir", "__files", "__conn"]
+	#__slots__ = ["__root_dir", "__files", "__root"]
 	
 	def __init__(self, root_dir):
-		self.__root_dir = root_dir
+		self.__root_dir = os.path.normpath(root_dir)
 		self.__files = None
+		self.__root = Directory()
 		self.load()
 		self.update()
 		self.save()
+		
+	@property
+	def root(self):
+		return self.__root
 		
 	@property
 	def cache_path(self):
@@ -30,7 +67,23 @@ class CachedFileIndex(object):
 			self.__files = {}
 		
 	def update(self):
-		pass
+		for base_dir, dirs, files in os.walk(self.__root_dir, topdown=True):
+			base_path = self.split_path(base_dir)
+			dir = self.__root
+			for path_elem in base_path:
+				if len(path_elem) == 0:
+					continue
+				dir = dir.get_dir(path_elem)
+			for file in files:
+				file_path = os.sep.join((base_dir, file))
+				file_stat = os.stat(file_path)
+				mtime = file_stat.st_mtime
+				size = file_stat.st_size
+				dir.add_file(File(file, mtime, size, 0))
+			
+	def split_path(self, dir):
+		dir = dir[len(self.__root_dir)+1:]
+		return dir.split(os.sep)
 	
 	def save(self):
 		cache_file = open(self.cache_path, "wb")
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import core.files, core.file_index
 import sys
 
+def walk(dir, indent=""):
+	print indent, "D", dir.name
+	print indent, "=" * (len(str(dir.name))+2)
+	for file in dir.files.values():
+		print indent, "F", file
+	for subdir in dir.dirs.itervalues():
+		walk(subdir, indent + "-")
+
 if __name__ == "__main__":
 	file_index = core.file_index.CachedFileIndex(sys.argv[1])
+	walk(file_index.root)
